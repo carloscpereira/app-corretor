0e6c45705d5eb0a1ef9d44a88bfe0b7fd867df23
Carlos Cesar <carlos.csp@icloud.com>
2023-11-17 09:42:08 -0300

    first commit

--- conexao.php
+++ conexao.php
@@ -1,2 +1,2 @@
 <?php
-$con = pg_connect("host=endereco_server_banco port=porta dbname=nome_do_banco user=usuario password=senha") or die("não acessou");
+$con = pg_connect("host=192.168.0.101 port=5435 dbname=dbportais_prd_v1 user=postgres password=5RdxsW2@") or die("não acessou");
